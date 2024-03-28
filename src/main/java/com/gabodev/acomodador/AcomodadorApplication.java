e9378c9478a6a37cc41f99aca396f70811ea8c0f
Gabodev <51827577+Gguardiola@users.noreply.github.com>
2024-03-28 10:23:40 +0100

    Initial Commit

---
+++ src/main/java/com/gabodev/acomodador/AcomodadorApplication.java
@@ -0,0 +1,13 @@
+package com.gabodev.acomodador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcomodadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcomodadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabodev/acomodador/AcomodadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabodev.acomodador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcomodadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
